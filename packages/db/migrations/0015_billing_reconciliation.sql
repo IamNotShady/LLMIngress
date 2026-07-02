3ddfe7b4c1abbf0acb3081b1f5ae4a13b30aef7c
Ethan <zhou.xiaoxiao@outlook.com>
2026-07-02 15:04:29 +0800

    chore: compress v1 delivery history

--- packages/db/migrations/0015_billing_reconciliation.sql
+++
@@ -1,5 +0,0 @@
-insert into schema_version (id, version)
-values (1, '0015')
-on conflict (id) do update
-set version = '0015',
-    updated_at = now();
--- packages/db/migrations/0035_virtual_model_description_schema_version.sql
+++
@@ -1,5 +0,0 @@
-insert into schema_version (id, version)
-values (1, '0035')
-on conflict (id) do update
-set version = '0035',
-    updated_at = now();
